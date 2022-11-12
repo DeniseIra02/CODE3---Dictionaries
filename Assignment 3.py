f34673f1d8a4f5e3a195890240c4788b82545703
DeniseIra02 <irajubilo@gmail.com>
2022-11-12 11:15:17 +0800

    Started displaying the Menu options

---
+++ Assignment 3.py
@@ -0,0 +1,10 @@
+#Menu
+print("-----------MENU------------")
+print()
+print("     1 -> Add an item")
+print("     2 -> Search")
+print("     3 -> Exit")
+print()
+print("---------------------------")
+
+#Ask user what to do
