7b160f39721c3f6effb25570841fe7a2eded1c5e
DeniseIra02 <irajubilo@gmail.com>
2022-11-12 13:02:42 +0800

    Eliminate errors at this point but the display result in option 2 is in dictionary form.

--- Assignment 3.py
+++ Assignment 3.py
@@ -26,13 +26,13 @@ while True:
         print()
         user_gender = input("Enter your Gender. \n Gender: ") 
         print()
-        user_age = int(input("Enter your Age. \n Age:"))
+        user_age = input("Enter your Age. \n Age: ")
         print()
         user_bdate = input("Enter your birthdate. \n Birthdate: ")
         print()
         user_add = input("Enter your Address. \n Address: ")
         print()
-        user_phoneNumber = int(input("Enter your Phone Number. \n Phone Number: "))
+        user_phoneNumber = input("Enter your Phone Number. \n Phone Number: ")
         print()
         user_vacc = input("Enter what is your Vaccine. \n Vaccine: ")
         print()
@@ -41,8 +41,8 @@ while True:
         print("Saved!")
         print()
         print("*****************************")
-
-    #Declaring dictionary
+        
+        #Declaring dictionaries
         user_info = {
             "Full Name": user_fullName,
             "Gender": user_gender,
@@ -53,13 +53,20 @@ while True:
             "Vaccine": user_vacc,
             "Vaccine Status": user_vaccstat
         }
-
-    #codings for option 2
-    if input_of_user == 2:
-        input_name2 = input("Enter your Fullname. \n Full Name: ")
-        if user_fullName == input_name2:
-            for key, value in user_info.items:
+        
+        input_of_user2 = int(input("What do you want to do? (Choose 1-3): "))
+        print("*****************************")
+        print()
+        
+        #codings for option 2
+        if input_of_user2 == 2: 
+            user_fullName2 = input("Enter your Fullname. \n Full Name: ")
+            if user_fullName == user_fullName2:
                 print()
-                print("*********USER INFO**********")
+                print("**********USER INFO**********")
                 print()
-                print(key,":", value)
+                print(user_info)
+                print("*****************************")
+            else:
+                print("*****************************")
+                print("!Invalid User Name!")
